bbba6cdfc8cdb61e17e1275ac3081634fc1c1cac
Joel Myers <joeldmyers@gmail.com>
2018-12-15 09:05:39 -0500

    optimizing get, adding set, insert and remove methods to doubly linked list

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -91,25 +91,62 @@ class DoublyLinkedList {
       return undefined;
     }
 
-    let count = 0;
-    let current = this.head;
 
-    while (count !== index) {
-      current = current.next;
-      count++;
+    if (index <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      let count = this.length;
+      let current = this.tail;
+
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
     }
+
     return current;
   }
 
-  set(val) {
-
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
   }
 
-  insert(val) {
+  insert(index, val) {
+    (index < 0 || index > this.length) && return false;
+    (index === 0) && return this.unshift(val);
+    (index === this.length) && return this.push(val);
 
+    const newNode = new Node(val);
+    const nodeBefore = this.get(index - 1);
+    const nodeAfter = nodeBefore.next;
+    newNode.prev = nodeBefore;
+    newNode.next = nodeAfter;
+    nodeAfter.prev = newNode;
+    nodeBefore.next = newNode;
+    this.length++;
+    return true;
   }
 
-  remove(val) {
-
+  remove(index) {
+    (index < 0 || index >= length) && return false;
+    (index === 0) && return this.shift();
+    (index === this.length - 1) && return this.pop();
+    const nodeToRemove = this.get(index);
+    nodeToRemove.prev.next = nodeToRemove.next;
+    nodeToRemove.next.prev = nodeToRemove.prev;
+    nodeToRemove.prev = null;
+    nodeToRemove.next = null;
+    this.length--;
+    return this;
   }
 }
