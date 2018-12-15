01fab43091efb6a1350b9e36316c682c406bf7cd
Joel Myers <joeldmyers@gmail.com>
2018-12-15 08:50:53 -0500

    adding pop, shift, unshift, and unoptimized get methods to doubly linked list

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -31,19 +31,74 @@ class DoublyLinkedList {
   }
 
   pop(val) {
+    if (this.length === 0) {
+      return undefined;
+    }
 
+    const poppedNode = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return this;
   }
 
-  shift(val) {
+  shift() {
+    // removes first element from array
+
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    var oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.head = this.head.next;
+    this.head.prev = null;
+    oldHead.next = null;
+    this.length--;
+    return this;
 
   }
 
   unshift(val) {
-
+    // adds value to start
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    length++;
+    return this;
   }
 
-  get(val) {
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
 
+    let count = 0;
+    let current = this.head;
+
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
   }
 
   set(val) {
