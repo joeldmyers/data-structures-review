99bb15da5130ed8d7e410bc39058549f12474a75
Joel Myers <joeldmyers@gmail.com>
2018-12-15 08:38:31 -0500

    Adding doubly linked list and push method

---
+++ data-structures/doubly-linked-list.js
@@ -0,0 +1,60 @@
+class Node(val) {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.tail = newNode;
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  pop(val) {
+
+  }
+
+  shift(val) {
+
+  }
+
+  unshift(val) {
+
+  }
+
+  get(val) {
+
+  }
+
+  set(val) {
+
+  }
+
+  insert(val) {
+
+  }
+
+  remove(val) {
+
+  }
+}
