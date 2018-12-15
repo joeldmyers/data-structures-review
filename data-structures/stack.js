9b816f3633ad28062633deef8d8cda371fd96371
Joel Myers <joeldmyers@gmail.com>
2018-12-15 09:31:27 -0500

    implementing stack class

---
+++ data-structures/stack.js
@@ -0,0 +1,50 @@
+// Implementation of stack using array
+// last in, first out
+
+
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var newNode = newNode;
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+      this.size++;
+      return this.size;
+    }
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return null;
+    } else {
+
+      const temp = this.first;
+      this.first.next = null;
+
+      if (this.size === 1) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return temp.val;
+    }
+  }
+}
